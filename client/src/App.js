9cce53c888df4da5a8c056d2b8c2939eb020cb0d
leejunghoo <leejunghoo515@naver.com>
2021-11-29 13:56:13 +0900

    상단바 구현

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+import './App.css';
+import Topbar from './Topbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar></Topbar>
+      <hr></hr>
+    </div >
+  );
+}
+
+export default App;
