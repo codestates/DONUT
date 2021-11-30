32703d59dc2bba9a4d1b75ea3e529dea3307f928
CHOIHYERYUN <65389841+CHOIHYERYUN@users.noreply.github.com>
2021-11-30 16:00:22 +0900

    Update Auth.js

--- client/src/Auth.js
+++ client/src/Auth.js
@@ -5,9 +5,9 @@ import qs from "qs";
 import { useNavigate } from "react-router-dom";
 
 const Auth = () => {
-  const REST_API_KEY = "80707c3a0383bc1cc8f54a77a631fdb3";
-  const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
-  const CLIENT_SECRET = "cbZmJGLQBACLpJmPUiWzv7GbArJAgGtS";
+  const REST_API_KEY = "";
+  const REDIRECT_URI = "";
+  const CLIENT_SECRET = "";
 
   const [userEmail, setUserEmail] = useState();
   const [userName, setUserName] = useState();
