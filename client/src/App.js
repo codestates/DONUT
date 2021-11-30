624d1695b7777af1cd203fcd8f57de8cfb0bf0c4
leejunghoo <leejunghoo515@naver.com>
2021-11-30 15:06:08 +0900

    Feat MyInfo

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ import "./App.css";
 import Topbar from "./Topbar";
 import LoginPage from "./Page/LoginPage";
 import RenderPage from "./Page/RenderPage";
-
+import MyInfoPage from "./Page/MyInfoPage";
+import MyInfoEdit from "./Page/MyInfoEdit";
 function App() {
   return (
     <div>
@@ -16,6 +17,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<RenderPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/my" element={<MyInfoPage />}></Route>
+        <Route path="/my/my_info_Edit" element={<MyInfoEdit />}></Route>
       </Routes>
     </div>
   );
