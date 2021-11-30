94229bc74b80f0094b797bb1090f34c7c549f5f1
kyungsunnee <83901698+kyungsunnee@users.noreply.github.com>
2021-11-30 15:39:54 +0900

    feat: kakao profile / access_token 받기

---
+++ client/src/Auth.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import qs from "qs";
+import { useNavigate } from "react-router-dom";
+
+const Auth = () => {
+  const REST_API_KEY = "80707c3a0383bc1cc8f54a77a631fdb3";
+  const REDIRECT_URI = "http://localhost:3000/oauth/kakao/callback";
+  const CLIENT_SECRET = "cbZmJGLQBACLpJmPUiWzv7GbArJAgGtS";
+
+  const [userEmail, setUserEmail] = useState();
+  const [userName, setUserName] = useState();
+  const [profileImg, setProfileImg] = useState();
+
+  const code = new URL(window.location.href).searchParams.get("code");
+
+  const importProfile = (info) => {
+    // console.log(info);
+    setUserEmail(info.email);
+    setUserName(info.profile.nickname);
+    setProfileImg(info.profile.profile_image_url);
+  };
+
+  const history = useNavigate();
+
+  const getToken = async () => {
+    const payload = qs.stringify({
+      grant_type: "authorization_code",
+      client_id: REST_API_KEY,
+      redirect_uri: REDIRECT_URI,
+      code: code,
+      client_secret: CLIENT_SECRET,
+    });
+
+    console.log("payload", payload);
+
+    try {
+      // access token 가져오기
+      const res = await axios.post(
+        "https://kauth.kakao.com/oauth/token",
+        payload
+      );
+      // Kakao Javascript SDK 초기화
+      window.Kakao.init(REST_API_KEY);
+      // access token 설정
+      window.Kakao.Auth.setAccessToken(res.data.access_token);
+
+      let data = await window.Kakao.API.request({
+        url: "/v2/user/me",
+        success: (res) => {
+          const kakaoAccount = res.kakao_account;
+          console.log(
+            kakaoAccount.email,
+            kakaoAccount.profile.nickname,
+            kakaoAccount.profile.profile_image_url
+          );
+          importProfile(kakaoAccount);
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getToken();
+  }, []);
+
+  console.log(userEmail, userName, profileImg);
+
+  return null;
+};
+export default Auth;
