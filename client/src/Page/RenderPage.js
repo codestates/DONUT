5d743501a600d7df3fbda80e12246fd1f779e661
kyungsunnee <83901698+kyungsunnee@users.noreply.github.com>
2021-12-03 14:45:43 +0900

    fix : 렌더페이지 로그인페이지

--- client/src/Page/RenderPage.js
+++ client/src/Page/RenderPage.js
@@ -1,10 +1,32 @@
 import React from "react";
+import qs from "qs";
+import axios from "axios";
 
 function RenderPage({ isLogin, setIsLogin }) {
   // console.log("렌더페이지", isLogin);
-  const code = new URL(window.location.href).searchParams.get("code");
-  console.log(code);
+  const url = new URL(window.location.href);
+  const authorizationCode = url.searchParams.get("code");
 
+  const getAccessToken = (authorizationCode) => {
+    //console.log(authorization); // 잘찍힘
+    axios
+      .post(
+        "https://localhost:4000/KakaoCallback",
+        qs.stringify({ authorizationCode }),
+        {
+          headers: {
+            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+          },
+        }
+      )
+      .then((data) => {
+        //console.log(data, "이거 맞음?")
+      });
+  };
+
+  if (authorizationCode) {
+    getAccessToken(authorizationCode);
+  }
   return <section></section>;
 }
 
