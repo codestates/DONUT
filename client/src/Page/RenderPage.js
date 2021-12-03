80bbe52fc4a4cb09ff41d0839bb013234d470ca3
kyungsunnee <83901698+kyungsunnee@users.noreply.github.com>
2021-12-03 11:34:15 +0900

    feat: LoginPage 로그인 기능, fix: RenderPage.js / App.js

--- client/src/Page/RenderPage.js
+++ client/src/Page/RenderPage.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-function RenderPage() {
+function RenderPage({ isLogin, setIsLogin }) {
+  // console.log("렌더페이지", isLogin);
+  const code = new URL(window.location.href).searchParams.get("code");
+  console.log(code);
 
-  return <section></section>
+  return <section></section>;
 }
 
 export default RenderPage;
