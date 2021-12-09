212b3f21a80db349deef62d92e3745914d43868a
julie <hlee748@gmail.com>
2021-12-09 16:46:40 +0900

    fix:포스트 업로드 api

--- client/src/Page/PostUploadPage.js
+++ client/src/Page/PostUploadPage.js
@@ -1,17 +1,21 @@
-import { faTags } from '@fortawesome/free-solid-svg-icons';
 import React, { useState } from "react"
 import './PostUploadPage.css'
 import axios from 'axios';
 
 
-
 function PostUploadPage() {
 	const defaultFileName = "이미지 파일을 업로드 해주세요.";
-	const [content, setContent] = useState("");
 	const [file, setFile] = useState(null);
 	const [imgSrc, setImgSrc] = useState(null);
 	const [fileName, setFileName] = useState(defaultFileName);
-	const [percent, setPercent] = useState(0);
+	const [allContent, setAllContent] = useState({ 
+		picture: "",
+		writing: ""
+	})
+
+	const handleChangeValue = (key) => (e) =>{
+		setAllContent({ ...allContent, [key]: e.target.value})
+	}
 
 	const imageSelectHandler = (e) => {
 		const imageFile = e.target.files[0]
@@ -22,37 +26,55 @@ function PostUploadPage() {
 		fileReader.onload = (e) => setImgSrc(e.target.result)
 	};
 
+	const postToServer = async (data) => {
+		setAllContent({...allContent, picture: data})
+		//console.log(allContent)
+		await axios.post("http://localhost:4000/AddPost", allContent,
+		{
+			headers: {
+			  "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+			},
+		})
+		.then(res => console.log(res))
+		.catch(err => console.log(err))
+	}
+
 	const onSubmit = async(e) => {
 		e.preventDefault();
+
+		const content = {picture: allContent.picture,
+				 writing: allContent.writing}
+
+		// if( !content.picture || !content.writing){
+		// 	alert('이미지를 업로드 하거나 내용을 입력하세요')
+		// }
+
 		const formData = new FormData();
 		formData.append('image', file);
-		// for(let key of formData.keys()){
-		// 	console.log(key)
-		// }
-		// formData.append('content', content)
+
 		try{
 			const res = await axios.post("https://localhost:4000/upload", formData, 
-			{headers: {"Content-type": "multipart/form-data"},
-			onUploadProgress: (e) => {
-				setPercent(Math.round(100 * e.loaded/e.total))
-			}
-		});
+			{headers: {
+					"Content-type": "multipart/form-data"},
+			})
+			.then(res => postToServer(res.data.data))
+			.catch(err => console.log(err))
+		//console.log(allContent)
+
+		
+
+
 		setTimeout(() => {
-			setPercent(0);
 			setFileName(defaultFileName);
 			setImgSrc(null)
 		}, 3000)
-			console.log({res})
+			//console.log({res})
 		}catch(err){
-			setPercent(0);
 			setFileName(defaultFileName);
 			setImgSrc(null)
 			console.log(err)
 		}
-	}
-
-	const contentChangeHandler = (e) => {
-		setContent(e.target.value)
+		// window.location.replace("https://localhost:3000/Post")
 	}
 
 
@@ -62,13 +84,12 @@ function PostUploadPage() {
 			<div className="post-title">POST</div>
 			<form onSubmit={onSubmit}>
 				<img src={imgSrc} className={`image-preview ${imgSrc && "image-preview-show"}`}/>
-				{percent}
 				<div className= "file-dropper">
 					{fileName}
 					<input id="image" type="file" accept="image/*" onChange={imageSelectHandler} />
 					{/* <img src="https://cdn.discordapp.com/attachments/907148581692137515/917214337951727617/imageUp.png" alt="" style={{width: 400, height: 400}}/> */}
 				</div>
-				<textarea className="post-text-area" onChange={contentChangeHandler} placeHolder="content">{content}</textarea>
+				<textarea className="post-text-area" onChange={handleChangeValue('writing')} placeHolder="content"></textarea>
 				<button type="submit" className="post-btn">SHARE</button>
 			</form>
 				
