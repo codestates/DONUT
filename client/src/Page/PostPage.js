dc71c6862d6f526fe619fcdea4c91232a10a6a19
julie <hlee748@gmail.com>
2021-12-11 15:36:31 +0900

    feat: 사진 업로드 렌더링

--- client/src/Page/PostPage.js
+++ client/src/Page/PostPage.js
@@ -2,17 +2,18 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { faCamera } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import PostList from "./DummyPostList";
 import "./PostPage.css";
 import axios from 'axios';
+
 
 function PostPage() {
 	const [postList, setPostList] = useState([]);
 
 	useEffect(() => {
 		axios.get("https://localhost:4000/AllPost")
-		.then((res) => setPostList(postList.concat(res.data.data)))
+		.then((res) => setPostList(res.data.data))
 	}, []);
+
 	
 	const MoveToSinglePost = () => {
 		
@@ -26,7 +27,7 @@ function PostPage() {
 	    		<span className="camera-icon"><FontAwesomeIcon icon={faCamera} onClick={MoveToSinglePost} size="2x" /></span>
 		    </Link>
 		    <div className="posts">
-			{postList.map((el) => (<div className="post-image"><img src={el.image} alt=""/></div>))}
+			{postList.map((el) => (<div className="post-image"><img src={`https://localhost:4000/${el.picture}`} alt=""/></div>))}
 		    </div>	
 		
 	</div>
