9b971125b0a2b459267fe36838699520123c71b0
HyeRyun Choi <hyeryunchoi@HyeRyunui-MacBookPro.local>
2021-12-14 15:43:06 +0900

    fix: before

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -3,7 +3,7 @@
 # I want to make sure that the directory is clean and has nothing left over from
 # previous deployments. The servers auto scale so the directory may or may not
 # exist.
-if [ -d /home/ubuntu/DONUT/server ]; then
-    rm -rf /home/ubuntu/DONUT/server
+if [ -d /home/ubuntu/DONUT/ ]; then
+    rm -rf /home/ubuntu/DONUT/
 fi
-mkdir -vp /home/ubuntu/DONUT/server
+mkdir -vp /home/ubuntu/DONUT/
