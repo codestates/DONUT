3c545646712bd929794590d0d1b32e10f6238359
kyungsunnee <83901698+kyungsunnee@users.noreply.github.com>
2021-12-16 15:50:06 +0900

    fix: FreetalkPage CSS3

--- client/src/Page/FreeTalkWrite.js
+++ client/src/Page/FreeTalkWrite.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import qs from "qs";
-import SendLoginPage from "./SendLoginPage";
 import "./FreetalkPage.css";
 axios.defaults.withCredentials = true;
 
@@ -41,10 +40,9 @@ function FreeTalkWrite({ isLogin, setIsLogin }) {
 
   return (
     <>
-      {isLogin ? (
-        <div className="write-board">
-          <h1>FreeTalk</h1>
-          {/* <div className="wirte-container">
+      <div className="write-board">
+        <div className="free-talk-name">FREE TALK</div>
+        {/* <div className="wirte-container">
         {saveContent.map((element) => (
           <div>
             <h2>{element.title}</h2>
@@ -52,40 +50,35 @@ function FreeTalkWrite({ isLogin, setIsLogin }) {
           </div>
         ))}
       </div> */}
-          <div className="form-wrapper">
+        <div className="form-wrapper">
+          <div className="free-talk-wirte-div">
             <div className="free-talk-wirte-div">
-              <div className="free-talk-wirte-div">
-                <input
-                  className="title-input"
-                  type="text"
-                  placeholder="제목"
-                  onChange={inputContent("title")}
-                  name="title"
-                />
-              </div>
-              <div className="free-talk-wirte-div">
-                <textarea
-                  type="text"
-                  className="text-area"
-                  placeholder="글을 적어주세요"
-                  onChange={inputContent("content")}
-                  style={{ fontsize: "1em" }}
-                ></textarea>
-              </div>
-              <div className="free-talk-wirte-div button">
-                <button
-                  className="submit-button"
-                  onClick={uploadContentHandler}
-                >
-                  완료
-                </button>
-              </div>
+              <input
+                className="title-input"
+                type="text"
+                placeholder="제목"
+                onChange={inputContent("title")}
+                name="title"
+              />
+            </div>
+            <div className="free-talk-wirte-div">
+              <textarea
+                type="text"
+                className="text-area"
+                placeholder="글을 적어주세요"
+                onChange={inputContent("content")}
+                style={{ fontsize: "1em" }}
+              ></textarea>
+            </div>
+            <div className="button-div">
+              <button className="submit-button" onClick={uploadContentHandler}>
+                완료
+              </button>
             </div>
           </div>
         </div>
-      ) : (
-        <SendLoginPage />
-      )}
+      </div>
+      )
     </>
   );
 }
--- server/controllers/freetalk/AllFreetalk.js
+++ server/controllers/freetalk/AllFreetalk.js
@@ -3,29 +3,27 @@ const { freetalk, comment } = require("../../models");
 
 module.exports = async (req, res) => {
   // 클라이언트에서 get 요청 => 비회원도 이용할 수 있기 때문에 토큰 확인은 필요없음(단순 열람)
-  // findAll
+
   try {
     // const commentCounter = async (id) => {
     //   let count = 0;
     //   const comments = await comment.findAll({
     //     where: { freetalkId: id },
     //   });
-    //   count = comments.length ? Number(comments.length) : 0;
-    //   return count;
+    //   count = comments.length ? comments.length : 0;
     // };
 
     const wholeFreetalk = await freetalk.findAll({
       order: [["createdAt", "DESC"]],
     });
 
-    // wholeFreetalk.forEach((e) => {
-    //   commentCounter(e.dataValues.id).then((data) => {
-    //     e.dataValues.commentCount = data;
-    //   });
-    //   console.log(e.dataValues.commentCount);
+    const comments = await comment.findAll({});
+
+    // comments.forEach((e) => {
+    //   console.log(e.dataValues);
     // });
-    // console.log(wholeFreetalk);
-    res.status(200).json({ data: wholeFreetalk });
+
+    res.status(200).json({ data: wholeFreetalk, comment: comments });
   } catch (err) {
     return res.status(501).json({ message: "서버 에러" });
   }
